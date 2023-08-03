87f901490a6584e182fc1d56f40e8e27c5f2c3ec
Jero <jerocaru@gmail.com>
2023-08-03 11:46:01 +0200

    second commit

--- src/main/java/org/example/pluralsight/Main.java
+++ src/main/java/org/example/pluralsight/Main.java
@@ -1,7 +1,17 @@
 package org.example.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Haciendo una prueba en git");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe cualquier cosa");
+        String line = sc.nextLine();
+
+        System.out.println(line);
+
     }
 }
